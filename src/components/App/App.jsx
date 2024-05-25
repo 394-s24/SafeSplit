390db2edf504d5f4efa75d0a608241acba230c2c
Zyanik <106106326+geperez8@users.noreply.github.com>
2024-05-24 21:32:02 -0500

    Fixed index.js and added testing file

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,16 +4,15 @@ import { ref, onValue } from "firebase/database";
 import { useEffect } from "react";
 import RideForm from "../RideForm/RideForm.jsx";
 import DataLogger from "../DataLogger/DataLogger.jsx";
-import { Container, Spinner, Row} from "react-bootstrap";
+import { Container, Spinner, Row } from "react-bootstrap";
 import "./App.css";
 import NavBar from "../NavBar/NavBar.jsx";
-
+import { ChakraProvider } from "@chakra-ui/react";
 
 const App = () => {
-  const [FirebaseData, setFirebaseData] = useState();
-  const [key, setKey] = useState('request');
+  const [FirebaseData, setFirebaseData] = useState({"requests":[], "matches":[]});
+  const [key, setKey] = useState("request");
   const [user, setUser] = useState("johnsmith@gmail.com");
-
 
   useEffect(() => {
     const firebaseRef = ref(db);
@@ -25,122 +24,125 @@ const App = () => {
   let foundMaxMatchId = 0;
   let foundMaxRequestId = 0;
 
-  // Make sure FirebaseData has loaded and contains data
-  if (!FirebaseData) {
-    return (
-      <Container className="spinner-container">
-        <Row>
-          <Spinner animation="border" role="primary">
-            <span className="visually-hidden">Loading...</span>
-          </Spinner>
-        </Row>
-      </Container>
-    );
-  }
+  // // Make sure FirebaseData has loaded and contains data
+  // if (!FirebaseData) {
+  //   return (
+  //     <Container className="spinner-container">
+  //       <Row>
+  //         <Spinner animation="border" role="primary">
+  //           <span className="visually-hidden">Loading...</span>
+  //         </Spinner>
+  //       </Row>
+  //     </Container>
+  //   );
+  // }
 
-  var reqData = Array();
-  // fixed for nonconsecutive indices
-  for (const [key, value] of Object.entries(FirebaseData["requests"])) {
-    foundMaxRequestId < parseInt(key)
-      ? (foundMaxRequestId = parseInt(key))
-      : null;
+  if (FirebaseData) {
+    var reqData = Array();
+    // fixed for nonconsecutive indices
+    for (const [key, value] of Object.entries(FirebaseData["requests"])) {
+      foundMaxRequestId < parseInt(key)
+        ? (foundMaxRequestId = parseInt(key))
+        : null;
 
-    const {
-      email,
-      locationFrom,
-      locationTo,
-      numRiders,
-      requestTimeStart,
-      requestTimeEnd,
-      status,
-      match_id,
-    } = value;
-
-    if (email == user) {
-      var matchStart = new Date(requestTimeStart * 1000).toLocaleString();
-      var matchEnd = new Date(requestTimeEnd * 1000).toLocaleString();
-      reqData.push([
-        matchStart,
-        matchEnd,
+      const {
+        email,
         locationFrom,
         locationTo,
-        user,
         numRiders,
+        requestTimeStart,
+        requestTimeEnd,
         status,
-        key,
         match_id,
-      ]);
+      } = value;
+
+      if (email == user) {
+        var matchStart = new Date(requestTimeStart * 1000).toLocaleString();
+        var matchEnd = new Date(requestTimeEnd * 1000).toLocaleString();
+        reqData.push([
+          matchStart,
+          matchEnd,
+          locationFrom,
+          locationTo,
+          user,
+          numRiders,
+          status,
+          key,
+          match_id,
+        ]);
+      }
+      reqData.sort((a, b) => new Date(a[0]) - new Date(b[0]));
     }
-    reqData.sort((a, b) => new Date(a[0]) - new Date(b[0]));
-  }
 
-  var matchData = Array();
-  // fixed for nonconsecutive indices
-  for (const [key, value] of Object.entries(FirebaseData["matches"])) {
-    foundMaxMatchId < parseInt(key) ? (foundMaxMatchId = parseInt(key)) : null;
+    var matchData = Array();
+    // fixed for nonconsecutive indices
+    for (const [key, value] of Object.entries(FirebaseData["matches"])) {
+      foundMaxMatchId < parseInt(key)
+        ? (foundMaxMatchId = parseInt(key))
+        : null;
 
-    const {
-      timeStart,
-      timeEnd,
-      locationFrom,
-      locationTo,
-      rider1,
-      rider2,
-      rider3,
-    } = value;
-
-    let riderArr = Array();
-
-    rider1 ? riderArr.push(rider1) : null;
-    rider2 ? riderArr.push(rider2) : null;
-    rider3 ? riderArr.push(rider3) : null;
-
-    if (riderArr.includes(user)) {
-      var matchStart = new Date(timeStart * 1000).toLocaleString();
-      var matchEnd = new Date(timeEnd * 1000).toLocaleString();
-      matchData.push([
-        matchStart,
-        matchEnd,
+      const {
+        timeStart,
+        timeEnd,
         locationFrom,
         locationTo,
-        riderArr,
-        key,
-      ]);
-    }
+        rider1,
+        rider2,
+        rider3,
+      } = value;
 
-    matchData.sort((a, b) => new Date(a[0]) - new Date(b[0]));
+      let riderArr = Array();
+
+      rider1 ? riderArr.push(rider1) : null;
+      rider2 ? riderArr.push(rider2) : null;
+      rider3 ? riderArr.push(rider3) : null;
+
+      if (riderArr.includes(user)) {
+        var matchStart = new Date(timeStart * 1000).toLocaleString();
+        var matchEnd = new Date(timeEnd * 1000).toLocaleString();
+        matchData.push([
+          matchStart,
+          matchEnd,
+          locationFrom,
+          locationTo,
+          riderArr,
+          key,
+        ]);
+      }
+
+      matchData.sort((a, b) => new Date(a[0]) - new Date(b[0]));
+    }
   }
   // Accessing just the rider1 field from the first object in FirebaseData
   // console.log(FirebaseData)
-  // max id for new request 
-  foundMaxRequestId++
-  
+  // max id for new request
+  foundMaxRequestId++;
+
   return (
-    <div>
-       <NavBar
-        user={user}
-        setUser={setUser}
-      />
-    <Container>
-      <RideForm
-        currMaxId={foundMaxRequestId}
-        currMaxMatchId={foundMaxMatchId}
-        data={FirebaseData}
-        tabKey={key}
-        setTabKey = {setKey}
-        user = {user}
-      />
-      <Row>
-        <DataLogger
-          reqData={reqData}
-          matchData={matchData}
-          tabKey={key}
-          setTabKey={setKey}
-          firebaseData={FirebaseData}
-        />
-      </Row>
-      </Container>
-    </div>
+    <ChakraProvider>
+      <div>
+        <NavBar user={user} setUser={setUser} />
+        <Container>
+          <RideForm
+            currMaxId={foundMaxRequestId}
+            currMaxMatchId={foundMaxMatchId}
+            data={FirebaseData}
+            tabKey={key}
+            setTabKey={setKey}
+            user={user}
+          />
+          <Row>
+            <DataLogger
+              reqData={reqData}
+              matchData={matchData}
+              tabKey={key}
+              setTabKey={setKey}
+              firebaseData={FirebaseData}
+            />
+          </Row>
+        </Container>
+      </div>
+    </ChakraProvider>
   );
 };
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/App/App';
-// 1. import `ChakraProvider` component
-import { ChakraProvider } from '@chakra-ui/react'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./components/App/App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <ChakraProvider>
   <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </ChakraProvider>
-  
+    <App />
+  </React.StrictMode>
 );

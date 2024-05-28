7b7d2f2faca93ce4bf8837f8b89d30d903ec1d59
Zyanik <106106326+geperez8@users.noreply.github.com>
2024-05-27 19:36:22 -0500

    Added some tests

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,16 +4,49 @@ import App from './components/App/App';
 
 describe('counter tests', () => {
     
-  test("Counter should be 0 at the start", () => {
+  test("we should see the submit button for the form", () => {
     render(<App />);
-    expect(screen.getByText('count is: 0')).toBeDefined();
+    expect(screen.findAllByText("Submit form")).toBeDefined();
+  });
+
+
+  test("We should see the user logged in", () => {
+    render(<App />);
+    expect(screen.findAllByText("johnsmith@gmail.com")).toBeDefined();
+  });
+
+  test("we should see the delete button for ", () => {
+    render(<App />);
+    expect(screen.findAllByText("Delete")).toBeDefined();
+  });
+
+
+
+});
+
+describe("Data Logger", () => {
+  test("We should see the requests", async () => {
+    render(<App />);
+    const requestsTab = screen.getByText('Request');
+    fireEvent.click(requestsTab);
+    expect(requestsTab).toBeDefined();
+    // expect(await screen.getByText('janedoe@gmail.com')).toBeDefined();
+  });
+
+  test("We should see the Matches tab", async () => {
+    render(<App />);
+    const matchesTab = screen.getByText('Matches');
+    fireEvent.click(matchesTab);
+    expect(matchesTab).toBeDefined();
+    // expect(await screen.getByText('janedoe@gmail.com')).toBeDefined();
   });
 
   test("Counter should increment by one when clicked", async () => {
     render(<App />);
-    const counter = screen.getByRole('button');
-    fireEvent.click(counter);
-    expect(await screen.getByText('count is: 1')).toBeDefined();
+    const matchesTab = await screen.getByText('Matches');
+    fireEvent.click(matchesTab);
+    expect(matchesTab).toBeDefined();
+    expect(await screen.getByText('janedoe@gmail.com')).toBeDefined();
   });
 
 });

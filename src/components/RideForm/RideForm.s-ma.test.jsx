c1e722b7399e75fcb89989cd2f8d2c9823a9f2e2
stephaniiema <54160019+stephaniiema@users.noreply.github.com>
2024-06-06 18:29:42 -0500

    tests - stephanie 2

--- src/components/RideForm/RideForm.s-ma.test.jsx
+++ src/components/RideForm/RideForm.s-ma.test.jsx
@@ -1,6 +1,7 @@
 import {beforeEach, describe, expect, test, it, vi } from "vitest";
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import * as util from '../../utilities/FireBase';
+// import { ref, onValue, s} from "firebase/database";
 // import App from "../App/App";
 import RideForm from "./RideForm";
 import runAlgorithm from "./RideForm";
@@ -66,31 +67,98 @@ describe("Invalid date form test - success", () => {
 
 });
 
-// describe("Switch account and submit", () => {
-//     test("Submit request under different users", async () => {
-//         vi.spyOn(util, 'useAuthState').mockReturnValue([null]);
-//         render(<RideForm />);
+
+const mockUser = {
+    name : "Steph",
+    email: "steph@mock.com"
+};
+  
+const mockUser2 = {
+    name : "Bobb",
+    email: "bobb@mock.com"
+  };
+
+var curr_email = "";
+
+function set(ref, data)
+{
+    if ( "email" in data ){ 
+        expect(data["email"]).toEqual(curr_email);
+    }
+}
+
+describe("Switch account and submit -- success", () => {
+    test("Submit request under different users", async () => {
+
+        const mock = vi.fn().mockImplementation(set);
+
+        vi.spyOn(util, 'useAuthState').mockReturnValue(mockUser);
+        var curr_email = mockUser.email;
+        render(<RideForm />);
         
-//         const pickupLocation = screen.getByTestId("pickupLoc");
-//         const dropOffLocation = screen.getByTestId("dropoffLoc");
-//         const pickupDateE = screen.getByTestId("pickupDateE");
-//         const pickupTimeE = screen.getByTestId("pickupTimeE");
-//         const pickupDateL = screen.getByTestId("pickupDateL");
-//         const pickupTimeL = screen.getByTestId("pickupTimeL");
-//         const submitForm = screen.getByTestId("submit-button");
+        const pickupLocation = screen.getByTestId("pickupLoc");
+        const dropOffLocation = screen.getByTestId("dropoffLoc");
+        const pickupDateE = screen.getByTestId("pickupDateE");
+        const pickupTimeE = screen.getByTestId("pickupTimeE");
+        const pickupDateL = screen.getByTestId("pickupDateL");
+        const pickupTimeL = screen.getByTestId("pickupTimeL");
+        const submitForm = screen.getByTestId("submit-button");
     
-//         fireEvent.change(pickupLocation, { target: { value: "Allison" } });
-//         fireEvent.change(dropOffLocation, {target: { value: "Midway" } });
-//         fireEvent.change(pickupDateE, {target: { value: "2025-01-01" } });
-//         fireEvent.change(pickupTimeE, { target: { value: "14:00" } });
-//         fireEvent.change(pickupDateL, {target: { value: "2025-01-01"} });
-//         fireEvent.change(pickupTimeL, { target: { value: "16:00" } });
-//         fireEvent.click(submitForm);
+        fireEvent.change(pickupLocation, { target: { value: "Allison" } });
+        fireEvent.change(dropOffLocation, {target: { value: "Midway" } });
+        fireEvent.change(pickupDateE, {target: { value: "2025-01-01" } });
+        fireEvent.change(pickupTimeE, { target: { value: "14:00" } });
+        fireEvent.change(pickupDateL, {target: { value: "2025-01-01"} });
+        fireEvent.change(pickupTimeL, { target: { value: "16:00" } });
+        fireEvent.click(submitForm);
 
+        vi.spyOn(util, 'useAuthState').mockReturnValue(mockUser2);
+        var curr_email = mockUser2.email;
 
-//         // vi.spyOn(util, 'useAuthState').mockReturnValue([mockUser]);
-//         expect(runAlgorithm).toHaveBeenCalledTimes(1);
+        fireEvent.change(pickupLocation, { target: { value: "Allison" } });
+        fireEvent.change(dropOffLocation, {target: { value: "Midway" } });
+        fireEvent.change(pickupDateE, {target: { value: "2025-01-01" } });
+        fireEvent.change(pickupTimeE, { target: { value: "14:00" } });
+        fireEvent.change(pickupDateL, {target: { value: "2025-01-01"} });
+        fireEvent.change(pickupTimeL, { target: { value: "16:00" } });
+        fireEvent.click(submitForm);
+    });
+})
+  
+describe("Switch account and submit -- fail", () => {
+    test("Submit request under same user = same email", async () => {
+
+        const mock = vi.fn().mockImplementation(set);
+
+        vi.spyOn(util, 'useAuthState').mockReturnValue(mockUser);
+        var curr_email = mockUser.email;
+        render(<RideForm />);
         
-//       // expect(await screen.findByText("Sign out")).toBeDefined();
-//     });
-//   })
+        const pickupLocation = screen.getByTestId("pickupLoc");
+        const dropOffLocation = screen.getByTestId("dropoffLoc");
+        const pickupDateE = screen.getByTestId("pickupDateE");
+        const pickupTimeE = screen.getByTestId("pickupTimeE");
+        const pickupDateL = screen.getByTestId("pickupDateL");
+        const pickupTimeL = screen.getByTestId("pickupTimeL");
+        const submitForm = screen.getByTestId("submit-button");
+    
+        fireEvent.change(pickupLocation, { target: { value: "Allison" } });
+        fireEvent.change(dropOffLocation, {target: { value: "Midway" } });
+        fireEvent.change(pickupDateE, {target: { value: "2025-01-01" } });
+        fireEvent.change(pickupTimeE, { target: { value: "14:00" } });
+        fireEvent.change(pickupDateL, {target: { value: "2025-01-01"} });
+        fireEvent.change(pickupTimeL, { target: { value: "16:00" } });
+        fireEvent.click(submitForm);
+
+        // vi.spyOn(util, 'useAuthState').mockReturnValue(mockUser2);
+        // var curr_email = mockUser2.email;
+
+        fireEvent.change(pickupLocation, { target: { value: "Allison" } });
+        fireEvent.change(dropOffLocation, {target: { value: "Midway" } });
+        fireEvent.change(pickupDateE, {target: { value: "2025-01-01" } });
+        fireEvent.change(pickupTimeE, { target: { value: "14:00" } });
+        fireEvent.change(pickupDateL, {target: { value: "2025-01-01"} });
+        fireEvent.change(pickupTimeL, { target: { value: "16:00" } });
+        fireEvent.click(submitForm);
+    });
+  })
